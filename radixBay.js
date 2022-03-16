703ce14a75d39af0806e6206c3c074377e26a8ee
SpencerMuniz <sbottomley44@gmail.com>
2022-03-16 14:48:04 -0700

    updated algorithm to run any string entered into function

--- radixBay.js
+++ radixBay.js
@@ -1,12 +1,35 @@
-const isBalanced = string =>{
-    if(string.length % 2 !== 0) return false;
+// const isBalanced = string =>{
+//     if(string.length % 2 !== 0) return false;
 
-    while(string.includes('{}') || string.includes('()') || string.includes('[]') || string.includes('""')){
-        string = string.replace('{}', '').replace('()', '').replace('[]', '').replace('""', '')
+//     while(string.includes('{}') || string.includes('()') || string.includes('[]') || string.includes('""')){
+//         string = string.replace('{}', '').replace('()', '').replace('[]', '').replace('""', '')
+//     }
+//     return string === '';
+// }
+
+// console.log(isBalanced("([])[]({})"));
+// console.log(isBalanced("([)]"));
+// console.log(isBalanced("((()")); 
+
+
+const validParen = str =>{
+    let stack = [];
+    for(i = 0; i < stack.length; i++){
+        let character = stack[stack.length - 1]
+        if(str[i] == "(" || str[i] == "{" || str[i] == "["){
+            stack.push(str[i])
+        }
+        else if((character == "(" && str[i] == ")") || 
+        (character == "{" && str[i] == "}") ||
+        (character == "[" && str[i] == "]")){
+            stack.pop()
+        }
+        else{
+            return false;
+        }
     }
-    return string === '';
+    return stack.length ? false : true
 }
 
-console.log(isBalanced("([])[]({})"));
-console.log(isBalanced("([)]"));
-console.log(isBalanced("((()")); 
+let test = "{}[]()((()))({)}"
+console.log(validParen(test));
